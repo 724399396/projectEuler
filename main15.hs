89ca3326eb7d7ecc2af5c2c51726ece8e31794a0
liww.li <liww.li@qunar.com>
2017-02-06 18:32:48 +0800

    solve 2

--- main15.hs
+++ main15.hs
@@ -1,7 +1,10 @@
+import Data.List
+import qualified Data.Set as S
+
 type Location = (Int,Int)
 
-boardX = 20
-boardY = 20
+boardX = 15
+boardY = 15
 
 stepBack :: Location -> [Location]
 stepBack (x,y) = [(x-1,y), (x,y-1)]
@@ -13,7 +16,7 @@ iteractStep :: Int -> [[Location]]
 iteractStep 0 = [[(boardX, boardY)]]
 iteractStep n = let left = iteractStep (n-1)
                 in
-                  [x:xs|xs <- left, x<- stepBack (head xs), legalStep x]
+                   [x:xs|xs <- left, x<- stepBack (head xs), legalStep x]
 
 main :: IO ()
-main = putStrLn $ show $ length $ iteractStep (boardX * boardY)
+main = putStrLn $ show $ length $ iteractStep (boardX + boardY)
--- main17.hs
+++ main17.hs
@@ -1,8 +1,18 @@
-num2DigitList :: Integer -> [Integer]
-num2DigitList 0 = []
-num2DigitList n = (mod n 10) : (num2DigitList $ quot n 10)
+numDescriptionMap = [(1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"), (6, "six"), (7, "seven"), (8, "eight"), (9, "nine"), (10, "ten"), (11, "eleven"), (12, "twelve"), (13, "thirteen"), (14, "fourteen"), (15, "fifteen"), (16, "sixteen"), (17, "seventeen"), (18, "eighteen"), (19, "nineteen")]
+
+secondBitDescriptionMap = [(2, "twenty"), (3, "thirty"), (4, "forty"), (5, "fifty"), (6, "sixty"), (7, "seventy"), (8, "eighty"), (9, "ninety")]
+
+lookupWithDefault m n = case lookup n m of
+      Nothing -> ""
+      Just x -> x
 
 num2Description :: Integer -> [Char]
-num2Description n = case lookup n [(1, "one"), (2, "two"), (3, "three"), (4, "four"), (5, "five"), (6, "six"), (7, "seven"), (8, "eight"), (9, "nine")] of
-  Nothing -> ""
-  Just(x) -> x
+num2Description n
+  | n < 20 = lookupWithDefault numDescriptionMap n 
+  | n < 100 = lookupWithDefault secondBitDescriptionMap (quot n 10) ++ "-" ++ lookupWithDefault numDescriptionMap (mod n 10)
+  | n < 1000 = lookupWithDefault numDescriptionMap (quot n 100) ++ " hundred" ++ case (mod n 100) of
+      m | m > 0 -> " and " ++ num2Description (mod n 100)
+      m | otherwise -> ""
+
+
+res = 11 + (sum $ map (length . filter (\x -> x /= '-' && x /= ' ')) $ map num2Description [1..999])
