a39542c9e5597b28752407c5c682848f8adf60e8
liww.li <liww.li@qunar.com>
2017-02-08 18:10:26 +0800

    finish 15

--- main15.hs
+++ main15.hs
@@ -1,20 +1,34 @@
-import Data.List
-import qualified Data.Set as S
+import qualified Data.Vector as V
 
 type Location = (Int,Int)
 
-boardX = 20
-boardY = 20
+boardSize = 20
 
-stepBack :: Location -> [Location]
-stepBack (x,y) = [(x-1,y), (x,y-1)]
+stepBack :: Location -> V.Vector Location
+stepBack (x,y) = V.fromList [(x-1,y), (x,y-1)]
 
 legalStep :: Location -> Bool
 legalStep (x,y) = x >= 0 && y >= 0
 
-iteractStep :: Int -> [Location]
-iteractStep 0 = [(boardX, boardY)]
-iteractStep n = [x|y <- iteractStep (n-1), x<- stepBack y, legalStep x]
+iteractStep :: Int -> V.Vector (V.Vector Location)
+iteractStep 0 = V.fromList [V.fromList [(boardSize, boardSize)]]
+iteractStep n = do
+  y <- iteractStep (n-1)
+  x <- stepBack (V.head y)
+  if (legalStep x)
+  then return $ V.cons x y
+  else fail "" 
 
-main :: IO ()
-main = putStrLn $ show $ length $ iteractStep (boardX + boardY)
+res = length $ iteractStep (2*boardSize)
+
+f :: (Int -> Integer) -> Int -> Integer
+f mf 0 = 1
+f mf n = toInteger n * (mf (n-1))
+
+f_list :: [Integer]
+f_list = map (f faster_f) [0..]
+
+faster_f :: Int -> Integer
+faster_f n = f_list !! n
+
+res2 = faster_f (2*boardSize) `div` (faster_f boardSize * faster_f boardSize)
