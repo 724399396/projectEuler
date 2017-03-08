b918f4e4f9f0309a757f4c150882c3a0d7a5778f
liww.li <liww.li@qunar.com>
2017-03-08 18:41:13 +0800

    start 60

--- main60.hs
+++ main60.hs
@@ -3,4 +3,9 @@ import Numbers
 
 numConcat x y = let xBits = num2Bits x
                     yBits = num2Bits y
-                in map bits2Num [xBits ++ yBits, yBits ++ xBits]
+                in bits2Num $ ((reverse xBits) ++ (reverse yBits))
+
+res =  head [(a,b,c,d,e)| a <- primes, b <- takeWhile (<a) primes, c <- takeWhile (<b) primes, d <- takeWhile (<c) primes, e <- takeWhile (<d) primes, all isPrime $ [numConcat x y|x <- [a,b,c,d,e], y <- [a,b,c,d,e], x /= y]]
+                            
+main :: IO ()
+main = print res
